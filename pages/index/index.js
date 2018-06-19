bb3d77f991fb825d99f3d62815543963a4095083
曾哥帅的不谈 <461153861@qq.com>
2018-06-19 18:15:35 +0800

    页面的数据分析、组件划分、设计状态

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,27 @@
 var app = getApp();
 
-Page({});
+Page({
+  /**
+   * 视图的数据
+   */
+  data: {},
+
+  /**
+   * 视图的事件处理函数
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 视图的生命周期函数
+   */
+  onLoad: function () {
+
+
+  },
+  
+  onReady: function () {
+
+  }
+});
